dd7784982b97a6c120fb1ddd7eecd2e83693c758
Mathias Barbier <mathbarbier07@gmail.com>
2023-10-06 16:25:28 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,31 +1,21 @@
-let button = document.getElementById("btn");
+const textInput = document.getElementById("textInput");
+const button = document.getElementById("btn");
+const board = document.getElementById("board");
 
-function request () {
+const fetchName = (textInput) => fetch("https://api.zippopotam.us/be/" + textInput.value);
 
-fetch("./script/files.json")
-    .then((response) => response.json())
-    .then((json) => {
-        const unlist = document.createElement("ul");
-        document.body.appendChild(unlist);
-        for (let i = 0; i < json.length; i++) {
-            const obj = json[i];
-            const list = document.createElement("li");
-            const list1 = document.createElement("li");
-            const list2 = document.createElement("li");
-            const list3 = document.createElement("li");
-            list.textContent = `Firstname : ${obj.firstname}`;
-            list1.textContent = `Name : ${obj.name}`;
-            list2.textContent = `Age : ${obj.age}`;
-            list3.textContent = `City : ${obj.city}`;
-            unlist.appendChild(list);
-            unlist.appendChild(list1);
-            unlist.appendChild(list2);
-            unlist.appendChild(list3);
-        }
-    })
-    .catch((error) => {
-        console.log("There was an error", error);
-    });
-}
-
-button.addEventListener("click", request);
+button.addEventListener("click", () => {
+    fetchName(textInput)
+	.then((response) => response.json())
+	.then((json) => {
+        console.log(json.country);
+        console.log(json.places);
+        console.log(json.zip);
+        const list = document.createElement("li");
+        list.textContent = `Country : ${json.country}`;
+        board.appendChild(list);
+	})
+	.catch((error) => {
+		console.log("There was an error!", error);
+	});
+});
